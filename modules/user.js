4201b6585c812d66fff3a4fb8bbfcb1fae0f4765
nsandeepa <nilupuls01@gmail.com>
2017-01-09 00:17:02 +0530

    2017-01-08 Login Functionality Completed

--- modules/user.js
+++ modules/user.js
@@ -1,6 +1,7 @@
 /**
  * Created by nilupul on 12/31/16.
  */
+//user object for login and signup
 var User_Login = function (_fname, _username) {
     this.fname = _fname;
     this.username = _username;
